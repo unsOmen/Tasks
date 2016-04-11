851164fdbc3470953bc483cf3f0876fcbec6e5f6
OmeN <unsomen0405@gmail.com>
2016-04-11 17:42:12 +0600

    add task's 11/04/2016 upd

--- src/practice_8/File/Catalogi.java
+++ src/practice_8/File/Catalogi.java
@@ -21,15 +21,15 @@ public class Catalogi {
     }
 
     public static void main(String[] args) {
-        File path = new File("D:");
-        String[] list;
-        if(args.length == 0)
-            list = path.list();
-        else
-            list = path.list(new DirFilter(args[0]));
-        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
-        for(String dirItem : list)
-            System.out.println(dirItem);
+        //File path = new File("D:");
+        //String[] list;
+        //if(args.length == 0)
+        //    list = path.list();
+        //else
+        //    list = path.list(new DirFilter(args[0]));
+        //Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        //for(String dirItem : list)
+        //    System.out.println(dirItem);
 
         fileData(new File(args[0]));
     }
